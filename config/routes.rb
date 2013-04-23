7ed4961d9e031a1f16c85c094b69a87873c992de
vishnu.kanduri <vishnu.kanduri@maxsoftware.com.au>
2013-04-23 12:23:25 +1000

    changed the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Omrails::Application.routes.draw do
+  get  'about' =>'pages#about'
+  root :to => 'Pages#home'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
