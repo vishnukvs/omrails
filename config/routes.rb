59b0917d82c62415d466b96afc82f444bfec6e78
vishnu <vishnu.kvs@gmail.com>
2013-04-24 20:43:58 +1000

    added header footer and twitter bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Omrails::Application.routes.draw do
+  get  'about'=>'Page#about'
+  root :to => 'Page#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
