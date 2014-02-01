4913a030d89a12acf8808c28b9d41b7871cc4db0
vishnu <vishnu.kvs@gmail.com>
2014-02-01 16:43:32 +1100

    added config.routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Omrails::Application.routes.draw do
-<<<<<<< HEAD
   get  'about'=>'Page#about'
   root :to => 'Page#home'
-=======
+
   resources :pins
 
 
@@ -13,7 +12,7 @@ Omrails::Application.routes.draw do
   root :to => 'Pages#home'
 
 
->>>>>>> 76b1720ed8a4f0e6c0544ccdc67d7a0ac6ee10f5
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
